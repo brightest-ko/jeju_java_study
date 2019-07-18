e5f78f85caeeaea4afe819ab290b3ba359c9e078
brightest-ko <brightest95hj@gmail.com>
2019-07-18 16:17:47 +0900

    Command Pattern

--- 20190718/Test057.java
+++ 20190718/Test057.java
@@ -12,9 +12,17 @@ class AddCalc implements ICalc{
 }
 public class Test057{
 	public static void main(String[] args) throws Exception{
-		ICalc i = new AddCalc(3);
-		int r = 5;
-		r = i.execute(r);
-		System.out.println(r);
+		ICalc[] ls = new ICalc[5];
+		ls[0] = new AddCalc(2);
+		ls[1] = new AddCalc(6);
+		ls[2] = new AddCalc(3);
+		ls[3] = new AddCalc(7);
+		ls[4] = new AddCalc(5);
+		
+		int y = 10;
+		for(int i=0;i<ls.length;i++){
+			y = ls[i].execute(y);
+			System.out.println(y);
+		}
 	}
 }
