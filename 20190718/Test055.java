182c0f394b00b7508df1c3a1ef82e4993204f3f1
brightest-ko <brightest95hj@gmail.com>
2019-07-18 15:53:03 +0900

    Auto Boxing

--- 20190718/Test055.java
+++ 20190718/Test055.java
@@ -3,7 +3,9 @@ public class Test055{
 	public static void main(String[] args) throws Exception{
 
 		Integer i = 100; //는 컴파일러가 Integer i= new Integer(100);로 자동으로 바꿈
+		Object t = 200;
 		System.out.println(i.getClass().getName());
+		System.out.println(t.getClass().getName());
 		
 	}
 }
