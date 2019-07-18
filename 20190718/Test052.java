0cec8cb19ff030671208cb1bd11f8bf7cd7137be
brightest-ko <brightest95hj@gmail.com>
2019-07-18 15:11:05 +0900

    Gemeric

---
+++ 20190718/Test052.java
@@ -0,0 +1,14 @@
+class Temp <T extends Object>{
+	private T data = null;
+	public T getData(){return data;}
+	public void setData(T data){this.data=data;}
+}
+
+public class Test052{
+	public static void main(String[] args) throws Exception{
+		Temp<String> t = new Temp<String>();
+		t.setData("Hello");
+		String l = t.getData();
+		System.out.println(l);
+	}
+}
