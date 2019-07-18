eb5d648a7dc395c62add0b0231951f8dd132795f
brightest-ko <brightest95hj@gmail.com>
2019-07-18 12:04:23 +0900

    배열

---
+++ 20190718/Test045.java
@@ -0,0 +1,11 @@
+public class Test045{
+	public static void main(String[] args) throws Exception{
+		int[] i = new int[]{1,2,3,4};
+		System.out.println(i[0]);
+		System.out.println(i[1]);
+		System.out.println(i[2]);
+		System.out.println(i[3]);
+		
+		System.out.println(i.length);
+	}
+}
