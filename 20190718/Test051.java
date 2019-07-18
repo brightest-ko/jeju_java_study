3253c57250c2984237b0285c6c2c5e8e61802c51
brightest-ko <brightest95hj@gmail.com>
2019-07-18 15:02:56 +0900

    String은 인스턴스 생성이 가능한 Class & Object형변환

--- 20190718/Test051.java
+++ 20190718/Test051.java
@@ -3,13 +3,26 @@ class Temp{
 	public Object getData(){return data;}
 	public void setData(Object data){this.data=data;}
 }
+class Temp2{
+	private String data = null;
+	public String getData(){return data;}
+	public void setData(String data){this.data=data;}
+}
 
 public class Test051{
 	public static void main(String[] args) throws Exception{
+	
+		//String만 가능하지만, 끄집어낼때 Casting할때 필요없다.
+		Temp2 t2 = new Temp2();
+		t2.setData("HelloWorld");
+		String l2 = t2.getData();
+		System.out.println(l2);
+		
+		//모든 자료형 가능하지만, 끄집어낼땐 Casting이 필요하다.
 		Temp t = new Temp();
 		t.setData("HelloWorld");
-		
 		String l = (String)t.getData();
+		System.out.println(l);
 		
 		t.setData(100);
 		System.out.println(t.getData());
