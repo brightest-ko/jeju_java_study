0f16880efcc6c7bb3be4ba8bb4f77a8ac77a6fce
brightest-ko <brightest95hj@gmail.com>
2019-07-18 14:17:12 +0900

    String

---
+++ 20190718/Test050.java
@@ -0,0 +1,12 @@
+class Temp{
+	private Object data = 100;
+	public Object getData(){return data;}
+	public void setData(Object data){this.data=data;}
+}
+
+public class Test049{
+	public static void main(String[] args) throws Exception{
+		String l = "HelloWorld";
+		System.out.println(l);
+	}
+}
