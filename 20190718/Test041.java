ada85a14819460c7f7f2f8289b1c50403706f639
brightest-ko <brightest95hj@gmail.com>
2019-07-18 10:24:22 +0900

    변수는 Overridding안됨 & Class Casting

--- 20190718/Test041.java
+++ 20190718/Test041.java
@@ -4,16 +4,20 @@ class A{
 
 class B extends A{
 	int i=200;
+	void print(){
+		System.out.println("B");
+	}
 }
 
 public class Test041{
 	public static void main(String[] arg){
 		A t = new B();
 		System.out.println(t.i);
+		//t.print(); //불가능
 		
 		// t 포인터가 가리키는 인스턴스를 감싸는 B클래스 인스턴스를 t2 가 가르킨다.
 		B t2 = (B)t;
 		System.out.println(t2.i);
-		
+		t2.print();
 	}
 }
