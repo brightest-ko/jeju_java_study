a5c6ea98a4310c80dd7cc24e13e513cf86f41682
brightest-ko <brightest95hj@gmail.com>
2019-07-19 11:47:16 +0900

    Boolean

--- 20190719/Test063.java
+++ 20190719/Test063.java
@@ -1,14 +1,17 @@
 public class Test063{
-	public static void main(String args[]){
+	public static void main(String[] args){
 		boolean i = true;
 		boolean j = false;
+		 
+		System.out.println(10 < 5);
+		System.out.println(10 / 3); //몫 (int / int = int)
+		System.out.println(10 % 3); //나머지 
 		
-		System.out.println( 10 < 5 );
-		System.out.println( 10 / 3 ); //
-		//비교연산은 boolean값을 리턴 : == != > < >= <=
-		//거의 모든 연산은 같은 자료형끼리 가능 : int/int=int
+		//double값은 정확하다고 할 수 없기 때문에 == 비교는 해서는 안된다. 
+		System.out.println(10.0 / 3);
+		System.out.println( 3.3333 * 3 == 10);
+		
+		//비교연산은 boolean 값을 리턴: == != < > <= >=
+		//거의 모든 연산은 같은 자료형 끼리 가능.
 	}
 }
-
-//C는 정수값은 0은 false, 0이 아닌 모든 정수는 true
-//if(100)
