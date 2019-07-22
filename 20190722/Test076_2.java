96611064aafbc6be64c332fdc60dda5004a0ce25
brightest-ko <brightest95hj@gmail.com>
2019-07-22 11:08:02 +0900

    Java.io Package

--- 20190722/Test076_2.java
+++ 20190722/Test076_2.java
@@ -4,13 +4,9 @@ import java.io.OutputStream;
 import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 
-public class Test077{
-	public static void main(String[] args){
-		try{
+public class Test076_2{
+	public static void main(String[] args) throws FileNotFoundException{
 			OutputStream out = new FileOutputStream("a.dat");
-		}catch(FileNotFoundException e){
-		
-		}
 	}
 }
 
